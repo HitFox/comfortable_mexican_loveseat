a2ee4fe58ffceea4f4f453c515541bc4c798c4ef
Adam Bahlke <adam.bahlke@hitfoxgroup.com>
2015-07-27 14:23:59 +0200

    adding template files for initializer and robots, writing up README

---
+++ templates/comfortable_mexican_loveseat.rb
@@ -0,0 +1,14 @@
+# Demo initializer
+# ComfortableMexicanLoveseat.setup do |config|
+#   config.data = OpenStruct.new
+#   config.data.seo = OpenStruct.new
+#   config.data.seo.custom_routes = [
+#     # Example:
+#     # {route: 'welcome/index', last_modified: 'YYYY-MM-DD'}
+#   ]
+#   config.data.seo.model_routes = [
+#     # Example:
+#     # { model: YourModelCamelCased, where_args: {key: value}}
+#     # { model: YourModelCamelCased, where_args: 'some sql statement'}
+#   ]
+# end
