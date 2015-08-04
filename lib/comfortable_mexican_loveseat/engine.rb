57a75e766a00018eb27c40a719236930c77e5f62
Adam Bahlke <adam.bahlke@hitfoxgroup.com>
2015-08-04 15:20:45 +0200

    update rack rewrite

--- lib/comfortable_mexican_loveseat/engine.rb
+++ lib/comfortable_mexican_loveseat/engine.rb
@@ -1,7 +1,7 @@
 module ComfortableMexicanLoveseat
   class Engine < ::Rails::Engine
     initializer "redirect trailing slash urls" do |app|
-      app.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
+      app.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
         r301 %r{^/(.*)/$}, '/$1'
       end
     end
