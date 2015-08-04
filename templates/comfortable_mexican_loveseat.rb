4c6d07eb1d28c15af351dd9a263a20e761a4987b
Adam Bahlke <adam.bahlke@hitfoxgroup.com>
2015-08-04 10:57:28 +0200

    change initializer template

--- templates/comfortable_mexican_loveseat.rb
+++ templates/comfortable_mexican_loveseat.rb
@@ -1,12 +1,12 @@
 # Demo initializer
-# ComfortableMexicanLoveseat.setup do |config|
-#   config.seo_custom_paths = [
-#     # Example:
-#     # {route: 'welcome/index', last_modified: 'YYYY-MM-DD'}
-#   ]
-#   config.seo_resource_paths = [
-#     # Example:
-#     # { resource_class: YourModelCamelCased, filter: {key: value}}
-#     # { resource_class: 'YourModelCamelCased', filter: 'some sql statement'}
-#   ]
-# end
+ComfortableMexicanLoveseat.setup do |config|
+  config.seo_custom_paths = [
+    # Example:
+    # {path: 'welcome/index', last_modified: 'YYYY-MM-DD'}
+  ]
+  config.seo_resource_paths = [
+    # Example:
+    # { resource_class: YourModelCamelCased, filter: {key: value}}
+    # { resource_class: 'YourModelCamelCased', filter: 'some sql statement'}
+  ]
+end
