d58975380cde64dc09494e818a899176bb1aea0c
itschn <lofd23@yahoo.de>
2015-08-12 11:15:02 +0200

    Refactorin

--- lib/comfortable_mexican_loveseat/fixture.rb
+++ lib/comfortable_mexican_loveseat/fixture.rb
@@ -1,7 +1,7 @@
 module ComfortableMexicanLoveseat::Fixture
   class Importer < ComfortableMexicanSofa::Fixture::Importer
     def initialize(from, to = from, force_import = false, locale = I18n.default_locale)
-      super
+      super(from, to, force_import)
       self.site = Comfy::Cms::Site.find_or_create_by(:identifier => to, :locale => locale)
     end
   end
