3fe0ff29f6d597b4dafc93012004788fc8cfe7a2
itschn <lofd23@yahoo.de>
2015-08-12 11:03:34 +0200

    Refactorin

--- lib/comfortable_mexican_loveseat.rb
+++ lib/comfortable_mexican_loveseat.rb
@@ -1,6 +1,7 @@
 require 'comfortable_mexican_sofa'
 require 'rack-rewrite'
 require 'comfortable_mexican_loveseat/engine'
+require 'comfortable_mexican_loveseat/fixture'
 
 module ComfortableMexicanLoveseat
   mattr_accessor :seo_custom_paths
