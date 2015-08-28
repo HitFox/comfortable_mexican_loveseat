87ecdddc41a60f9290fcc4bb7e68e2acb8b658eb
Adam Bahlke <adam.bahlke@hitfoxgroup.com>
2015-08-28 11:03:34 +0200

    remove twitter card

--- app/controllers/comfy/admin/cms/layouts_controller.rb
+++ app/controllers/comfy/admin/cms/layouts_controller.rb
@@ -56,7 +56,7 @@ protected
     @layout.app_layout  ||= @layout.parent.try(:app_layout)
     @layout.content     ||= "{{ cms:field:seo.old_page_url:string }}\n{{ cms:field:seo.meta_description:string }}\n{{ cms:field:seo.page_title:string }}\n{{ cms:field:seo.canonical_href:string }}\n{{ cms:field:seo.meta_index:string }}\n" +
                             "{{ cms:field:google_plus.name }}\n{{ cms:field:google_plus.description }}\n{{ cms:field:google_plus.image }}\n" + 
-                            "{{ cms:field:twitter.card }}\n{{ cms:field:twitter.site }}\n{{ cms:field:twitter.title }}\n{{ cms:field:twitter.description }}\n{{ cms:field:twitter.creator }}\n{{ cms:field:twitter.image_src }}\n" + 
+                            "{{ cms:field:twitter.site }}\n{{ cms:field:twitter.title }}\n{{ cms:field:twitter.description }}\n{{ cms:field:twitter.creator }}\n{{ cms:field:twitter.image_src }}\n" + 
                             "{{ cms:field:facebook.title }}\n{{ cms:field:facebook.type }}\n{{ cms:field:facebook.image }}\n{{ cms:field:facebook.description }}\n{{ cms:field:facebook.admins }}\n{{ cms:page:content:rich_text }}"
   end
 
