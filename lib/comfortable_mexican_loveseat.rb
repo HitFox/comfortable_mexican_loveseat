a44629ae96e0855a0b008a8be13174dc69eb911a
violettine <violettine@freenet.de>
2015-09-01 13:48:44 +0200

    deleted open_uri_redirect gem from files, not needed anymore

--- lib/comfortable_mexican_loveseat.rb
+++ lib/comfortable_mexican_loveseat.rb
@@ -1,6 +1,5 @@
 require 'comfortable_mexican_sofa'
 require 'rack-rewrite'
-require 'open_uri_redirections'
 require 'comfortable_mexican_loveseat/engine'
 require 'comfortable_mexican_loveseat/fixture'
 require 'comfortable_mexican_loveseat/cms_admin'
