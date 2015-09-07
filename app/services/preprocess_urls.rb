75408ce3ee5d06c156efcb795d3f9c77f690379c
violettine <violettine@freenet.de>
2015-09-07 10:41:21 +0200

    refactored get_http_and_code method and fixed a nil-bug

--- app/services/preprocess_urls.rb
+++ app/services/preprocess_urls.rb
@@ -9,13 +9,16 @@ class PreprocessUrls
   def run
     dup_key_url(@key_url)
     url_hash = fill_url_hash(@key_url)
+    puts 'hash_size'+url_hash.size.to_s
+    url_hash
   end
 
   def dup_key_url(key_url)
     # creates domain. and www.domain.
-    key_url.match(/(https?:\/\/)(www\.|)(\b\S+)[\.]/)
-    @www_key_url = $1+'www.'+$3+'.'
-    @non_www_key_url = $1+$3+'.'
+    if key_url.match(/(https?:\/\/)(www\.|)(\b\S+)[\.]/)
+      @www_key_url = $1+'www.'+$3+'.'
+      @non_www_key_url = $1+$3+'.'
+    end
   end
 
   def fill_url_hash(key_url)
@@ -39,17 +42,15 @@ class PreprocessUrls
       @body = ''
       @code = 0
       @redirect_target_url = nil
-      get_HTTP_and_code(@url_hash[parent_url][4].dup, 0, nil)
+      get_HTTP_and_code(@url_hash[parent_url][4].dup, 0)
       @url_hash[parent_url][2] = @code
-      if @redirect_target_url
+      if !@redirect_target_url.nil?
         @url_hash[parent_url][0] = 'untested'
         @url_hash[parent_url][5] = @redirect_target_url
         label_url(@redirect_target_url, 'redirect of-> '+parent_url)
       end
       if url_label(parent_url) == 'valid'
-        if @code == '404' || @code == 0
-          @url_hash[parent_url][0] = 'untested'
-        else
+        if @code == '200'
           doc = Nokogiri::HTML(@body)
           @url_hash[parent_url][3] = doc
           doc.xpath('//comment()').remove
@@ -59,13 +60,14 @@ class PreprocessUrls
               @child_url_array << child_url.to_s
             end
           end
+        else
+          @url_hash[parent_url][0] = 'untested'
         end
       end
     rescue Exception => e
       if e.message
-        @url_hash[parent_url][0] = e.message
-        @url_hash[parent_url][2] = e.message
-        @url_hash[parent_url][3] = 'empty'
+        @url_hash[parent_url][0] = '404 {'+e.message+'}'
+        @url_hash[parent_url][2] = '404 {'+e.message+'}'
         return false
       end
     end
@@ -93,63 +95,55 @@ class PreprocessUrls
     domain_url+url
   end
 
-  def get_HTTP_and_code(url, round, code)
-    begin
-      if round == 0
-        resp = Net::HTTP.get_response(URI.parse(url))
-      else
-        uri = URI.parse(url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true if uri.scheme == "https"
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        if round == 1
-          request = Net::HTTP::Get.new(uri.request_uri)
-        else
-          request = Net::HTTP::Get.new(uri.request_uri, 'Accept-Encoding' => 'identity')
-        end
-        resp = http.request(request)
-      end
-    rescue Exception => e
-      if round == 0
-        get_HTTP_and_code(url, 1, nil)
-      end
-      if round == 1
-        get_HTTP_and_code(url, 2, nil)
-      end
-      if round == 2
-        resp = nil
-      end
-    end
+  def get_HTTP_and_code(url, round)
+    code = nil
+
+    resp = perform_request(url, round)
     if !resp.nil? && %w{301 302 307}.include?(resp.code)
       redirect_url = resp.header['location']
       new_redirect_url = check_domain(redirect_url, url)
       if @redirect_target_url.nil?
         @redirect_target_url = new_redirect_url
       end
-      get_HTTP_and_code(new_redirect_url, 0, resp.code)
-    else
+      code = resp.code if code.nil?
+      perform_request(new_redirect_url, 0)
+    end
+    if @body.empty?
       if resp.nil?
-        if @body.empty?
-          @body = 'empty'
-          @code = 0
-        end
-        return
-      end
-      if code.nil?
-        if @body.empty?
-          @body = resp.body
-          @code = resp.code
-        end
-        return
-      end
-      if !code.nil?
-        if @body.empty?
-          @body = resp.body
-          @code = code
-        end
-        return
+        @body = 'empty'
+        @code = 0
+      else
+        @body = resp.body
+        @code = code.nil? ? resp.code : code
       end
     end
+  end
+
+  def perform_request(url, round)
+    puts url
+    uri = URI.parse(url)
+    begin
+      if round == 0
+        Net::HTTP.get_response(uri)
+      elsif round == 1
+        perform_request_with_cert_ignored(uri)
+      elsif round == 2
+        perform_request_with_cert_ignored(uri, 'Accept-Encoding' => 'identity')
+      else
+        nil
+      end
+    rescue Exception => e
+      puts 'exception'
+      perform_request(url, round + 1)
+    end
+  end
+
+  def perform_request_with_cert_ignored(uri, headers = {})
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == "https"
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri, headers)
+    http.request(request)
   end
 
   def url_label(url)
@@ -157,10 +151,12 @@ class PreprocessUrls
   end
 
   def label_url(child_url, parent_url)
+    puts 'label_url url: '+child_url
     if @url_hash[child_url].nil?
       if !child_url.ascii_only?
         @url_hash[child_url] = ['untested', parent_url]
       else
+        puts 'label_url url2: '+child_url
         case child_url
         when /mailto:/
           @url_hash[child_url] = ['system', parent_url]
