1ff5673c86efe37de7376b428225098ae39255ea
violettine <violettine@freenet.de>
2015-09-10 09:42:14 +0200

    added json_ld style (structured data specific), that is same_as and contactPoint to seo_script output

--- app/services/write_seo_snippet.rb
+++ app/services/write_seo_snippet.rb
@@ -3,6 +3,10 @@ class WriteSeoSnippet
   #   method gets all input of the three categories.
   # First check which categorie(s) are choosen
   #   then write each content in the right space of the script file
+
+  # Notes:
+  #   No redundant commas!
+  #   Contact case must have url or telephone to be valid!
   class << self
     def write_snippet(params)
       params = handle_params(params)
@@ -17,11 +21,11 @@ class WriteSeoSnippet
     end
 
     def seo_scripter(params)
-      content = fill_content(params)
+      logo, contact, profile = fill_content(params)
       seo_script =''
       seo_script << '<script type="application/ld+json">'
       seo_script << '{'
-      content.each do |k,v|
+      logo.each do |k,v|
         unless v.blank?
           if k == 'context' || k == 'type'
             seo_script << '"@'+k+'" : "'+v+'"'
@@ -31,32 +35,69 @@ class WriteSeoSnippet
           seo_script << ','
         end
       end
+  #   "contactPoint" : [{
+  #     "@type" : "ContactPoint",
+  #     "telephone" : "+1-401-555-1212",
+  #     "contactType" : "customer service"
+  #   }]
+      seo_script.chop!
+      seo_script << ',"contactPoint" : [{'
+      contact.each do |k,v|
+        unless v.blank?
+          seo_script << '"'+k+'" : "'+v+'"'
+          seo_script << ','
+        end
+      end
+      seo_script << '}]'
+  #   "sameAs" : [
+  #     "http://www.facebook.com/your-profile",
+  #     "http://www.twitter.com/yourProfile",
+  #     "http://plus.google.com/your_profile"
+  #   ]
+      seo_script << ',"sameAs" : ['
+      profile.each do |url|
+        unless url.blank?
+          seo_script << '"'+url+'"'
+          seo_script << ','
+        end
+      end
+      seo_script << ']'
       seo_script << '}'
-      seo_script.sub!(/,}/, '}')
       seo_script << '<script>'
-      puts seo_script
-      seo_script
+      new_seo_script = delete_script_errors(seo_script)
+      puts new_seo_script
+      new_seo_script
       #return '<script type="application/ld+json">{"@context": "http://schema.org","@type": "Organization","url" : "http://www.your-site.com","name" : "Your Organization Name"}<script>'
     end
 
     def fill_content(params)
-      content = {}
-      content['context'] = params[:context]
-      content['type'] = params[:type]
-      content['url'] = params[:url]
-      content['logo'] = params[:logo]
-      content['same_url'] = params[:same_url]
-      content['telephone'] = params[:telephone]
-      content['area'] = params[:area]
-      content['language'] = params[:language]
-      content['facebook_url'] = params[:facebook_url]
-      content['twitter_url'] = params[:twitter_url]
-      content['google_plus_url'] = params[:google_plus_url]
-      content['instagram_url'] = params[:instagram_url]
-      content['pinterest_url'] = params[:pinterest_url]
-      content['linkedin_url'] = params[:linkedin_url]
-      content['youtube_url'] = params[:youtube_url]
-      content
+      logo = {}
+      logo['context'] = params[:context]
+      logo['type'] = params[:type]
+      logo['url'] = params[:url]
+      logo['logo'] = params[:logo]
+      contact = {}
+      contact['url'] = params[:same_url]
+      contact['telephone'] = params[:telephone]
+      contact['contactType'] = params[:contact_type]
+      profile = []
+      profile << params[:facebook_url]
+      profile << params[:twitter_url]
+      profile << params[:google_plus_url]
+      profile << params[:instagram_url]
+      profile << params[:pinterest_url]
+      profile << params[:linkedin_url]
+      profile << params[:youtube_url]
+      return logo, contact, profile
+    end
+
+    def delete_script_errors(seo_script)
+      seo_script.sub!(/"sameAs" : \[\]/, '')
+      seo_script.sub!(/"contactPoint" : \[{}\]/, '')
+      seo_script.sub!(/,}]/, '}]')
+      seo_script.sub!(/,]/, ']')
+      seo_script.sub!(/,}/, '}')
+      seo_script
     end
 
   # logo:
