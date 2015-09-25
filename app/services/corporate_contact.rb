b98ab2a51cab2b5a3b62e04ca41ff2781c30514f
violettine <violettine@freenet.de>
2015-09-25 17:46:37 +0200

    hopefully last fix of validation and fix of corporate_contacts form style after error-submit render

--- app/services/corporate_contact.rb
+++ app/services/corporate_contact.rb
@@ -12,8 +12,14 @@ class CorporateContact
       message: 'only allows numbers with international country code prefix, like +49'},
       absence: { if: Proc.new{|u| u.contact_type.blank? },
         message: 'needs input of contact type' }
-  validates :contact_url, :area_served, :available_language,
+  validates :contact_url,
       absence: { if: Proc.new{|u| u.contact_type.blank? },
+        message: 'needs input of contact type' }
+  validates :area_served,
+      absence: { if: Proc.new{|u| !u.area_served.join.blank? && u.contact_type.blank? },
+        message: 'needs input of contact type' }
+  validates :available_language,
+      absence: { if: Proc.new{|u| !u.available_language.join.blank? && u.contact_type.blank? },
         message: 'needs input of contact type' }
   validates :contact_type,
       absence: { if: Proc.new{|u| u.telephone.blank? && u.contact_url.blank? },
